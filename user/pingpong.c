ab16dadad7476c0fd193c77eaabcf3f6de427af6
foliet <foliet@rainspace.cn>
2022-04-03 16:22:54 +0800

    feat:实现pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,4 +1,32 @@
 //
 // Created by foliet on 2022/4/3.
 //
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main() {
+    int p1[2];//parent->child
+    int p2[2];//child->parent
+    pipe(p1);
+    pipe(p2);
+    if (fork() == 0) {
+        //child
+        close(p1[0]);
+        close(p2[1]);
+        char* buffer = malloc(2);
+        read(p1[1],buffer,sizeof(buffer));
+        printf("%d: received ping\n",getpid());
+        write(p2[0],buffer,sizeof(buffer));
+        exit(0);
+    }
+    //parent
+    close(p1[1]);
+    close(p2[0]);
+    char buffer[] = {'x'};
+    read(p1[0],buffer,sizeof(buffer));
+    printf("%d: received pong\n",getpid());
+    write(p2[1],buffer,sizeof(buffer));
+    exit(0);
+}
 
