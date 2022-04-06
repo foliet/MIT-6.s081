77e939e9e846b2d513eddb9f44b707a633f05c60
foliet <foliet@rainspace.cn>
2022-04-06 22:32:23 +0800

    fix:修复pingpong的bug

--- user/pingpong.c
+++ user/pingpong.c
@@ -12,22 +12,17 @@ main() {
     pipe(p2);
     if (fork() == 0) {
         //child
-        close(p1[0]);
-        close(p2[1]);
-        char* buffer = malloc(2);
-        read(p1[1],buffer,sizeof(buffer));
-        fprintf(2,"%d: received ping\n",getpid());
-        write(p2[0],buffer,sizeof(buffer));
+        char buffer[] = {'y'};
+        read(p1[0], buffer, sizeof(buffer));
+        fprintf(2, "%d: received ping\n", getpid());
+        write(p2[1], buffer, sizeof(buffer));
         exit(0);
     }
     //parent
-    close(p1[1]);
-    close(p2[0]);
     char buffer[] = {'x'};
-    read(p1[0],buffer,sizeof(buffer));
-    fprintf(2,"%d: received pong\n",getpid());
-    write(p2[1],buffer,sizeof(buffer));
+    write(p1[1], buffer, sizeof(buffer));
     wait(0);
+    read(p2[0], buffer, sizeof(buffer));
+    fprintf(2, "%d: received pong\n", getpid());
     exit(0);
 }
-
